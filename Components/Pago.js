f1851b117d06cf76d2c5fbdd90bebfa0c4c5fa2a
Felipe Forero <felipe.forerome@gmail.com>
2019-04-12 20:33:32 -0500

    Agregar iconos Campos de texto disponibles Pasar información de perfil a la siguiente pantalla

--- Components/Pago.js
+++ Components/Pago.js
@@ -1,24 +1,78 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, TextInput, View, Button,ScrollView} from "react-native";
+import {StyleSheet, Text, TextInput, Image, Button, View, ScrollView} from "react-native";
 
 class Pago extends Component{
     constructor(props) {
         super(props);
-        this.state = { text: 'Useless Placeholder' };
+        this.state = {
+            textNombreApellido: 'Nombres y Apellidos',
+            textCorreo: 'Correo Eletrónico',
+            textIdentificacion: 'Identificación',
+            textNumTarjeta: 'Número de Tarjeta',
+            textFecha: 'Fecha Caducidad',
+        };
     }
+
+    calcularBingo=()=>{
+        let thisComp = this
+        const { average } = this.props;
+        thisComp.props.navigation.navigate('Preguntas',{
+            valorPerfil: average
+        })
+    }
+
     render(){
         const { navigation } = this.props;
         const valorPerfil = navigation.getParam('valorPerfil');
         return(
             <ScrollView>
+                <View style={styles.container}>
+                    <Image
+                        style = {styles.logo}
+                        source={require('../Assets/CubiM.png')}
+                    />
+                </View>
                 <TextInput
-                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
+                    style={{height: 40, marginHorizontal:'10%', marginBottom:'2.5%', marginTop: '25%', borderColor:'gray', borderWidth: 1}}
+                    onChangeText={(textNombreApellido) => this.setState({textNombreApellido})}
+                    value={this.state.textNombreApellido}
                 />
+                <TextInput
+                    style={{height: 40, marginHorizontal:'10%', marginBottom:'2.5%', borderColor:'gray', borderWidth: 1}}
+                    onChangeText={(textCorreo) => this.setState({textCorreo})}
+                    value={this.state.textCorreo}
+                />
+                <TextInput
+                    style={{height: 40, marginHorizontal:'10%', marginBottom:'2.5%', borderColor:'gray', borderWidth: 1}}
+                    onChangeText={(textIdentificacion) => this.setState({textIdentificacion})}
+                    value={this.state.textIdentificacion}
+                />
+                <TextInput
+                    style={{height: 40, marginHorizontal:'10%', marginBottom:'2.5%', borderColor:'gray', borderWidth: 1}}
+                    onChangeText={(textNumTarjeta) => this.setState({textNumTarjeta})}
+                    value={this.state.textNumTarjeta}
+                />
+                <TextInput
+                    style={{height: 40, marginHorizontal:'10%', marginBottom:'2.5%', borderColor:'gray', borderWidth: 1}}
+                    onChangeText={(textFecha) => this.setState({textFecha})}
+                    value={this.state.textFecha}
+                />
+                <Button title={"Pagar"}
+                        onPress={this.calcularBingo}/>
             </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: '40%'
+    },
+    logo: {
+        marginHorizontal: '40%'
+    },
+});
 
 export default Pago
